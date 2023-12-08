fad47dc35d89a015fa6ababfa9a3db0fc90f762b
Thosakki <thosakkikalkoken@gmail.com>
2023-12-08 23:59:05 +0000

    Partially working version of market assessment program.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-ALL	:	top-traded.csv
+ALL	:	top-traded.csv market-quality.csv
 
 sde.db	:	sde/fsd/typeIDs.yaml
 	rm -f sde.db && ./build-sde.py --initial
@@ -9,5 +9,12 @@ top-traded.csv	:	popular.csv top-market-items.py  sde.db aggregatecsv.csv.gz
 sde-TRANQUILITY.zip	:
 	curl -O https://eve-static-data-export.s3-eu-west-1.amazonaws.com/tranquility/sde.zip
 	unzip $@
+
+latest-orderset-by-station-type.csv.gz	:	latest.csv.gz
+	zcat $< | sort -t '	'  -k 9n -k 2n | gzip -9 - > $@
+
+
+market-quality.csv	:	latest-orderset-by-station-type.csv.gz top-traded.csv
+	zcat $< | ./calc-market-quality.py > $@
 
 .DELETE_ON_ERROR	:	top-traded.tsv
---
+++ calc-market-quality.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+
+from collections import namedtuple
+import csv
+import logging
+from typing import List, Optional
+import sqlite3
+import sys
+
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+log = logging.getLogger(__name__)
+
+ItemSummary = namedtuple('ItemSummary', ['ID', 'Name', 'GroupID', 'CategoryID', 'ValueTraded', 'Buy', 'Sell'])
+Order = namedtuple('Order', ['TypeID', 'StationID', 'IsBuy', 'Price', 'Volume'])
+StationInfo = namedtuple('StationInfo', ['ID', 'Name', 'SystemID', 'RegionID'])
+items = dict()
+
+con = sqlite3.connect("sde.db")
+cur = con.cursor()
+
+def mean(d: List[float]) -> float:
+    return(sum(d)/len(d))
+
+def parse_float(x: str) -> Optional[float]:
+    if x == '-':
+        return None
+    return float(x)
+
+# Read in most traded items.
+with open("top-traded.csv", "rt") as fh:
+    r = csv.DictReader(fh, delimiter=',')
+    for row in r:
+        try:
+            items[int(row['ID'])] = ItemSummary(ID=int(row['ID']), Name=row['Name'], GroupID=int(row['GroupID']), CategoryID=int(row['CategoryID']), ValueTraded=float(row['Value Traded']), Buy=parse_float(row['Buy']), Sell=parse_float(row['Sell']))
+        except ValueError as e:
+            raise RuntimeError("Failed to parse line {}: {}".format(row, e))
+
+def read_orderset():
+    r = csv.reader(sys.stdin, delimiter="\t")
+    for row in r:
+        # 911190994	41	2023-11-26T06:52:24Z	False	23572	23572	1	17.86	60000004	region	365	10000033	126876
+        _, typeID, _, is_buy, volume, _, _, price, stationID, _, _, _, _ = row
+        yield Order(TypeID=int(typeID), StationID=int(stationID), IsBuy=(is_buy=='True'), Price=float(price), Volume=int(volume))
+    yield Order(TypeID=0, StationID=0, IsBuy=False, Price=0, Volume=0)
+
+def get_station_info(stationID: int) -> StationInfo:
+    res = cur.execute("""
+    SELECT ID, Name, SystemID, RegionID
+    FROM Stations
+    WHERE ID = ?
+    """, [stationID])
+    r = res.fetchall()
+    if len(r) == 0:
+        log.info("Could not find station {}".format(stationID))
+        return None
+    if len(r) > 1:
+        raise RuntimeError("multiple values for a name from SDE")
+    row = r[0]
+    return StationInfo(row[0], row[1], row[2], row[3])
+
+def emit_station_stats(w, stationID: int, efficiencies: List[float]):
+    coverage = len(efficiencies) / len(items)
+    if len(efficiencies)>0:
+        mean_efficiency = mean(efficiencies)
+        eff_str = '{:.1f}%'.format((mean_efficiency-1)*100)
+    else:
+        eff_str = '-'
+    station_info = get_station_info(stationID)
+    w.writerow([str(stationID), station_info.Name if station_info is not None else "-", '{:.1f}%'.format(coverage*100), eff_str])
+
+
+w = csv.writer(sys.stdout)
+w.writerow(['StationID', 'Station Name', 'Coverage %', 'Inefficiency %'])
+current_station = None
+current_type = None
+current_station_price_efficiencies = None
+current_type_best_sell = None
+for x in read_orderset():
+    if current_type is not None and current_type != x.TypeID:
+        if current_type_best_sell is not None:
+            all_best_sell = items[current_type].Sell
+            if all_best_sell is None:
+                log.info("no best sell price for common item? {}".format(current_type))
+                efficiency = 1.0
+            else:
+                efficiency = current_type_best_sell / all_best_sell
+            current_station_price_efficiencies.append(efficiency)
+        current_type_best_sell = None
+
+    if current_station is None or current_station != x.StationID:
+        if current_station is not None:
+            emit_station_stats(w, current_station, current_station_price_efficiencies)
+        current_station_price_efficiencies = []
+        current_station = x.StationID
+
+    if x.TypeID in items and not x.IsBuy:
+        current_type = x.TypeID
+        if current_type_best_sell is None or current_type_best_sell > x.Price:
+            current_type_best_sell = x.Price
+
+# read_orderset() always pads the end with one bogus order line with a dummy item & station,
+# so we never have to emit the final item or station returned.
+
