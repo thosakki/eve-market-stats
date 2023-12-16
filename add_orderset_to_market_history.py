f62ee14dd06f5d765ba3ff49f31eb0d3a1b3ec30
Thosakki Kalkoken <thosakkikalkoken@gmail.com>
2023-12-16 08:12:47 +0000

    Filtering support. Some overall stats in the output.

--- add_orderset_to_market_history.py
+++ add_orderset_to_market_history.py
@@ -7,7 +7,7 @@ import datetime
 import gzip
 import logging
 import sqlite3
-from typing import Iterator, Tuple
+from typing import IO, Iterator, Set, Tuple
 
 import lib
 
@@ -25,7 +25,7 @@ class ItemMarket():
     Sell: float
     SellVolume: int
 
-def load(orderset_fname: str) -> Iterator[ItemMarket]:
+def load(orderset_fname: str, stations: Set[int]) -> Iterator[ItemMarket]:
     current_station = None
     current_item = None
     current_buy = None
@@ -38,7 +38,7 @@ def load(orderset_fname: str) -> Iterator[ItemMarket]:
             yield ItemMarket(TypeID=current_item, StationID=current_station, Buy=current_buy, Sell=current_sell, SellVolume=current_sell_vol)
             current_buy = current_sell = current_sell_vol = None
 
-        if o.StationID not in BEST_STATIONS: continue
+        if o.StationID not in stations: continue
         current_station = o.StationID
         current_item = o.TypeID
 
@@ -71,26 +71,51 @@ def init_db(conn: sqlite3.Connection):
     """)
 
 
+def read_filter_file(fh: IO) -> Set[int]:
+    """Returns the IDs from the given CSV."""
+    x = set()
+    r = csv.DictReader(fh)
+    for row in r:
+        x.add(int(row["ID"]))
+    return x
+
+
 def main():
     arg_parser = ArgumentParser(prog='add_orderset_to_market_history')
     arg_parser.add_argument('--initial', action='store_true')
     arg_parser.add_argument('--orderset', type=str)
+    arg_parser.add_argument('--filter_items', type=str)
+    arg_parser.add_argument('--extra_stations', nargs='*', type=int)
     args = arg_parser.parse_args()
 
     conn = sqlite3.connect("market-prices.db")
     if args.initial: init_db(conn)
+    stations = set(BEST_STATIONS)
+    stations.update(args.extra_stations)
+    log.info("Querying for stations {}".format(stations))
 
     oinfo = lib.OrdersetInfo(None, None)
     for o in lib.read_orderset_filter(args.orderset, oinfo):
         pass
-    logging.info("Orderset identified as {}, {}".format(oinfo.Orderset, oinfo.Date.date().isoformat()))
+    log.info("Orderset identified as {}, {}".format(oinfo.Orderset, oinfo.Date.date().isoformat()))
+
+    filter_items = None
+    if args.filter_items:
+        with open(args.filter_items) as fh:
+            filter_items = read_filter_file(fh)
+        log.info("Filtering based on {}: {} items selected".format(args.filter_items, len(filter_items)))
 
     count = 0
-    for im in load(args.orderset):
+    skipped = 0
+    for im in load(args.orderset, stations):
+        if filter_items is not None and im.TypeID not in filter_items:
+            skipped += 1
+            continue
         emit_item(conn, oinfo.Date.date(), im)
         count += 1
         if count % 1000 == 0: conn.commit()
     conn.commit()
+    log.info("Wrote {} items, skipped {} items".format(count, skipped))
 
 
 if __name__ == "__main__":
