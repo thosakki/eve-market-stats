6e8ad827f91e76e451ae6b6f6bc8634a10e10bdb
Thosakki Kalkoken <thosakkikalkoken@gmail.com>
2024-06-08 14:36:30 +0000

    Fix numbers with commas in from industry file.

--- load-industry.py
+++ load-industry.py
@@ -3,8 +3,9 @@
 from argparse import ArgumentParser
 import csv
 import lib
-import sqlite3
+import locale
 import logging
+import sqlite3
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -16,6 +17,7 @@ args = arg_parser.parse_args()
 
 sde = sqlite3.connect("sde.db")
 industryDB = sqlite3.connect("industry.db")
+locale.setlocale(locale.LC_NUMERIC, 'en_GB.UTF-8')
 
 if args.initial:
     industryDB.execute("""
@@ -40,7 +42,7 @@ with open(args.industry) as fh:
     cur = industryDB.cursor()
     reader = csv.DictReader(fh)
     for r in reader:
-        outputName = canonical_name(r["Thing to make"])
+        outputName = r["Thing to make"]
         outputInfo = lib.get_type_info_byname(sde, outputName)
         if outputInfo is None:
             log.warn("output {} not recognised".format(outputName))
@@ -50,6 +52,6 @@ with open(args.industry) as fh:
         if inputInfo is None:
             log.warn("input {} not recognised".format(inputName))
             continue
-        cur.execute("""INSERT OR REPLACE INTO BuildItems VALUES(?,?,?)""",[outputInfo.ID, outputInfo.Name, r["Quantity made"]])
-        cur.execute("""INSERT OR REPLACE INTO BuildItemInputs VALUES(?,?,?)""",[inputInfo.ID, outputInfo.ID, r["Quantity for N"]])
+        cur.execute("""INSERT OR REPLACE INTO BuildItems VALUES(?,?,?)""",[outputInfo.ID, outputInfo.Name, locale.atoi(r["Quantity made"])])
+        cur.execute("""INSERT OR REPLACE INTO BuildItemInputs VALUES(?,?,?)""",[inputInfo.ID, outputInfo.ID, locale.atof(r["Quantity for N"])])
     industryDB.commit()
